890fe5c279fdf589ab101091081d7fe956031810
penny <475215536@qq.com>
2016-09-03 16:27:24 +0800

    diyiban

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	//	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+var MainDirPath string
+var HtmlPath string
+
+func init() {
+	fmt.Println("start!")
+	MainFilePath, _ := filepath.Abs(os.Args[0])
+	MainDirPath = filepath.Dir(MainFilePath)
+	HtmlPath = MainDirPath + "/static/html/"
+}
+func main() {
+	fmt.Println("MainDir", MainDirPath)
+	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/chenquan/", proxychenquan)
+	http.HandleFunc("/sunqixiang/", proxysunqixiang)
+	http.HandleFunc("/youwei/", proxyyouwei)
+	http.HandleFunc("/jilingyan/", proxyjilingyan)
+	fmt.Println(http.ListenAndServe("172.17.1.172:80", nil))
+}
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	//w.WriteHeader(404)
+	bufs, _ := ioutil.ReadFile(HtmlPath + "index.html")
+	w.Write(bufs)
+}
+func proxychenquan(w http.ResponseWriter, r *http.Request) {
+	res, _ := http.Get("http://127.0.0.10" + r.URL.Path[9:]) ///省略去9位
+	buf, _ := ioutil.ReadAll(res.Body)
+	w.Write(buf)
+	res.Body.Close()
+}
+func proxysunqixiang(w http.ResponseWriter, r *http.Request) {
+	res, _ := http.Get("http://127.0.0.11" + r.URL.Path[11:]) ///省略去11位
+	buf, _ := ioutil.ReadAll(res.Body)
+	w.Write(buf)
+	res.Body.Close()
+}
+func proxyyouwei(w http.ResponseWriter, r *http.Request) {
+	res, _ := http.Get("http://127.0.0.12" + r.URL.Path[7:]) ///省略去7位
+	buf, _ := ioutil.ReadAll(res.Body)
+	w.Write(buf)
+	res.Body.Close()
+}
+
+func proxyjilingyan(w http.ResponseWriter, r *http.Request) {
+	res, _ := http.Get("http://127.0.0.13" + r.URL.Path[10:]) ///省略去10位
+	buf, _ := ioutil.ReadAll(res.Body)
+	w.Write(buf)
+	res.Body.Close()
+}
